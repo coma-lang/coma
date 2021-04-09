4a5aad34662a88a038cfa4e71869da547cf74843
Viktor A. Rozenko Voitenko <sharp.vik@gmail.com>
2021-04-09 11:39:09 +0100

    Feat(Parser): instance Show Coma

--- src/Coma.hs
+++ src/Coma.hs
@@ -1,8 +1,8 @@
 -- Module Coma contains the "eval" function -- the most important function in
 -- the codebase. It receives Coma source code as a String and returns another
--- String -- the CSV output of the script. This way, Main simply passes pure
--- data to us. However, our scipts require file-reading to happen in the process
--- and that is why we're returning "IO String" instead of just "String".
+-- String -- the output of the script. This way, Main simply passes pure data to
+-- us. However, our scipts require file-reading to happen in the process and 
+-- that is why we're returning "IO String" instead of just "String".
 
 
 module Coma (eval) where
@@ -13,8 +13,20 @@ import qualified Parser
 
 
 -- EVAL
--- Receive source code of a file and return CSV output.
+-- Receive source code of a file and return output. Sometimes, the output is not
+-- necessarily CSV, it can be any valid Parser.Coma expression displayed as
+-- String.
 
 
 eval :: String -> IO String
-eval = return . show . Parser.parse . Lexer.lex
+eval code = (exec $ Parser.parse $ Lexer.lex code) >>= (return . show)
+
+
+
+-- EXEC
+-- Interpreter function. Take Parser.Coma (AST), execute instructions, and 
+-- return simplified Parser.Coma.
+
+
+exec :: Parser.Coma -> IO Parser.Coma
+exec = return
--- syntax/Parser.y
+++ syntax/Parser.y
@@ -1,6 +1,7 @@
 { 
 module Parser where 
 import qualified Data.HashMap as HM
+import Data.List (intercalate)
 import Lexer 
 }
 
@@ -109,5 +110,25 @@ data Coma
   | Divide Coma Coma
   | Call Coma Coma
   | Let String Coma Coma
-  deriving Show
+  
+instance Show Coma where
+  show (IntAtom i) = show i
+  show (StrAtom s) = show s
+  show (Ident idt) = idt
+  show (List list) = "[ " ++ unwords (map show list) ++ " ]"
+  show (Lambda params expr) = 
+    "( " ++ intercalate " -> " params ++ " -> " ++ show expr ++ " )"
+  show (Equal e1 e2) = show e1 ++ " = " ++ show e2
+  show (NotEqual e1 e2) = show e1 ++ " != " ++ show e2
+  show (Less e1 e2) = show e1 ++ " < " ++ show e2
+  show (LessEqual e1 e2) = show e1 ++ " <= " ++ show e2
+  show (Greater e1 e2) = show e1 ++ " > " ++ show e2
+  show (GreaterEqual e1 e2) = show e1 ++ " >= " ++ show e2
+  show (Append e1 e2) = show e1 ++ " ++ " ++ show e2
+  show (Add e1 e2) = show e1 ++ " + " ++ show e2
+  show (Subtract e1 e2) = show e1 ++ " - " ++ show e2
+  show (Multiply e1 e2) = show e1 ++ " * " ++ show e2
+  show (Divide e1 e2) = show e1 ++ " / " ++ show e2
+  show (Call e1 e2) = show e1 ++ " " ++ show e2
+  show (Let idt e1 e2) = "let " ++ idt ++ " := " ++ show e1 ++ " in " ++ show e2
 }
