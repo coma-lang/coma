6222bc71075ba627da6db05c2ae42574a05bf7fb
Viktor A. Rozenko Voitenko <sharp.vik@gmail.com>
2021-05-07 12:45:56 +0100

    Submit(2): submit.sh script changed

--- submit.sh
+++ submit.sh
@@ -4,13 +4,12 @@
 
 folder=dist-submission
 
-./install.sh
-
 if [ -d $folder ]; then rm -rf $folder; fi
 mkdir $folder
 
-cp -r app src test coma.cabal README.md LICENSE ChangeLog.md hie.yaml $folder
-for n in {1..5}; do
+cp group.txt report.pdf $folder
+
+for n in {6..10}; do
     cp "problems/pr$n/pr$n.cql" "$folder/pr$n.cql"
 done
 
